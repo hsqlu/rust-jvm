897ef6f5d6ff1a094f3e9c659a6e17c71691e408
Qiannan Lyu <lvqiannan@gmail.com>
2020-03-09 21:45:47 +1300

    inital sample code dir

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,12 @@ fn main() {
                           .get_matches();
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+//    let config = matches.value_of("config").unwrap_or("default.conf");
+//    println!("Value for config: {}", config);
 
     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
     // required we could have used an 'if let' to conditionally get the value)
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+//    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 
     // Vary the output based on how many times the user used the "verbose" flag
     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
